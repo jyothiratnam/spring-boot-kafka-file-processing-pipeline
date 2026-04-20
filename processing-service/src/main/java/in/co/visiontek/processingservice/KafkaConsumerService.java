c4d4ca231c07186f78aa45f9d1b886617692328b
“JyothiRatnam” <jyothiratnam.mareedu@visiontek.co.in>
2026-04-20 10:35:03 +0530

    Initial commit: Kafka file processing pipeline with multi-service architecture

---
+++ processing-service/src/main/java/in/co/visiontek/processingservice/KafkaConsumerService.java
@@ -0,0 +1,36 @@
+package in.co.visiontek.processingservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumerService {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @KafkaListener(topics = "file_uploaded", groupId = "processing-group")
+    public void consume(String message) {
+
+        System.out.println("📥 Received: " + message);
+
+        try {
+            Thread.sleep(2000);
+
+            // 🔥 Modify message
+            String updatedMessage = message.replace("UPLOADED", "PROCESSED");
+
+            kafkaTemplate.send("file_processed", updatedMessage);
+
+            System.out.println("⚙️ Processing done");
+
+        } catch (Exception e) {
+
+            String failedMessage = message.replace("UPLOADED", "FAILED");
+
+            kafkaTemplate.send("file_failed", failedMessage);
+        }
+    }
+}
---
+++ upload-service/src/main/java/in/co/visiontek/uploadservice/FileEvent.java
@@ -0,0 +1,8 @@
+package in.co.visiontek.uploadservice;
+
+public class FileEvent {
+    private String fileId;
+    private String fileName;
+    private String filePath;
+    private String status;
+}
